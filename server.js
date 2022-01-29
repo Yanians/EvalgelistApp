ece87cee3bd49fbd9e6388ab446737c7e30de9d9
Yanians <brian25198928@gmail.com>
2022-01-29 11:45:52 +0800

    UPDATED PACKAGE

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 
-var express = require('express');
-var cons = require('consolidate');
-var app = express();
+// var express = require('express');
+// var cons = require('consolidate');
+// var app = express();
 
-app.engine('html',cons.handlebars);
-app.set('port',(process.env.PORT || 4000));
-app.set('view engine', 'html');
-app.set('views', __dirname + '/public');
-app.use(express.static('public'));
+// app.engine('html',cons.handlebars);
+// app.set('port',(process.env.PORT || 4000));
+// app.set('view engine', 'html');
+// app.set('views', __dirname,'.','/public');
+// app.use(express.static('public'));
 
-app.get('/*', function (req, res) {
-  res.render('app',{title: 'decode App', logo:'decode', message: '<span>decode App is running!</span>'});
-});
+// app.get('/*', function (req, res) {
+//   res.render('app',{title: 'decode App', logo:'decode', message: '<span>decode App is running!</span>'});
+// });
  
-app.listen(app.get('port'),function(){
-	console.log("Server is running at localhost:4000");
-});
+// app.listen(app.get('port'),function(){
+// 	console.log("Server is running at localhost:4000");
+// });
+
+const express = require('express');
+
+const path = require('path');
+      
+const app = express();
+
+const publicPath = path.join(__dirname,'.','public');
+
+const port = process.env.PORT || 9000;
+
+      app.use(express.static(publicPath));
+
+      app.get('/*', function(req, res){
+            res.sendFile(path.join(publicPath,'app.html'));
+      });
+
+      app.listen(port,()=> {
+      	console.log(`Server is running on port ${port}. `);
+      });
